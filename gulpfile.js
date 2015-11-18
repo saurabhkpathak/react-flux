cbedf15748138dc024adf3b0ad77b05a7122cab6
saurabhkpathak <saurabhkpathak@yahoo.com>
2015-11-18 15:47:50 +0530

    updating the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var open = require('gulp-open');
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream');
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 };
 
@@ -28,6 +33,16 @@ gulp.task('open', ['connect'], function() {
     .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}));
 });
 
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(connect.reload());
+});
+
 gulp.task('html', function() {
     gulp.src(config.paths.html)
     .pipe(gulp.dest(config.paths.dist))
@@ -36,6 +51,7 @@ gulp.task('html', function() {
 
 gulp.task('watch', function() {
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
