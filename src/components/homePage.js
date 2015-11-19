e0e82b935eb0ad6f474da0d745ba3bb71ba9e62d
saurabhkpathak <saurabhkpathak@yahoo.com>
2015-11-19 12:24:22 +0530

    creating new component

---
+++ src/components/homePage.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div className="jumbotron">
+                <h1>This is an Author Details App</h1>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
