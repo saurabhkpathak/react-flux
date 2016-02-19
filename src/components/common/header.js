f8d97a0c407a7de7025f36d3d8043c436f09e95d
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-19 13:04:37 +0530

    adding header jsx

---
+++ src/components/common/header.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var React = require('react');
+
+var Header = React.createClass({
+    render: function() {
+        return (
+            <header>This is a header</header>
+        );
+    }
+})
+
+module.exports = Header;
