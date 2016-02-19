561c4999a4b5bccdc2a0a1d0bcbd90283b09f300
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-19 17:36:37 +0530

    adding navigation links to header

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,11 +1,16 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Header = React.createClass({
     render: function() {
         return (
-            <header>This is a header</header>
+            <header className='clearfix'>
+                <span className="col-md-4"><Link to='about'>About</Link></span>
+                <span className="col-md-4"><Link to='app'>Home</Link></span>
+            </header>
         );
     }
 })
