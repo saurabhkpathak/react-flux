fe897caba33a4c773210e323ae62e99a936f1109
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-19 16:34:31 +0530

    impementing routing using react router

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+
+var routes = (
+    <Route name='app' path='/' handler={require('./components/app')}>
+        <DefaultRoute handler={require('./components/homePage')} />
+        <Route name='about' handler={require('./components/aboutPage')} />
+    </Route>
+);
+
+module.exports = routes;
