b88943509137441643077bf1ec1a7b550b35e078
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-19 13:03:27 +0530

    providing functionality for custom routing

--- src/main.js
+++ src/main.js
@@ -2,5 +2,32 @@ $ = jQuery = require('jquery');
 
 var React = require('react');
 var Home = require('./components/homePage');
+var About = require('./components/aboutPage');
+var Header = require('./components/common/header');
 
-React.render(<Home />, document.getElementById('app'));
+var ApiWrapper = require('./components/api/apiFile');
+
+var App = React.createClass({
+    render: function() {
+        var Child, source = 'http://jsonplaceholder.typicode.com/posts';
+        if (this.props.route === 'about') {
+            Child = About;
+        } else {
+            Child = Home;
+        }
+        return (
+            <div>
+                <Header />
+                <Child source={source} />
+            </div>
+        )
+    }
+});
+
+function render() {
+    var route = window.location.hash.substr(1);
+    React.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', render);
+render();
