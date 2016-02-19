629ed0f9187408f7068f31dd373bfebc58c94b74
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-19 13:03:54 +0530

    adding about page functionality

---
+++ src/components/aboutPage.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var React = require('react');
+var ApiWrapper = require('../components/api/apiFile');
+
+var About = React.createClass({
+    getInitialState: function() {
+        return {
+            data: undefined
+        };
+    },
+    renderAsyncData: function() {
+        var data, self = this;
+        ApiWrapper.getAllPosts().done(function(response) {
+            data = response;
+            self.setState({
+                data: data
+            });
+        }).fail(function(error) {
+            data = error;
+            self.setState({
+                data: data
+            });
+        });
+    },
+    componentDidMount: function() {
+        this.renderAsyncData();
+    },
+    shouldComponentUpdate: function(nextProps, nextState){
+        return this.state.data !== nextState.data;
+    },
+    render: function() {
+        if (this.state.data && this.state.data.length) {
+            return (
+                <div className="jumbotron">
+                    <p>About Page</p>
+                    <ul>
+                        {this.state.data.map(function(current) {
+                            return <li key={current.id}>{current.title}</li>
+                        })}
+                    </ul>
+                </div>
+            );
+        } else {
+            return (
+                <div className="jumbotron">
+                    No result found.
+                </div>
+            );
+        }
+    }
+});
+
+module.exports = About;
