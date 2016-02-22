cede336360ffd81853872afeb28d136cbd294e46
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-22 13:58:49 +0530

    getting initial values from api and adding onchange handler

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -4,11 +4,36 @@ var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
 var EditUser = require('./editUser');
+var ApiWrapper = require('./api/apiFile');
 
 var ManageUser = React.createClass({
+    getUserDetails: function() {
+        var self = this;
+        ApiWrapper.getUserById(2).done(function(data) {
+            self.setState({
+                user: data
+            });
+        }).fail(function(data) {
+            self.setState({
+                user: data
+            });
+        });
+    },
+    getInitialState: function() {
+        this.getUserDetails();
+        return {
+            user: {}
+        }
+    },
+    setUser: function(event) {
+        var field = event.target.name;
+        var value = event.target.value;
+        this.state.user[field] = value;
+        this.setState({user: this.state.user});
+    },
     render: function() {
         return (
-            <EditUser />
+            <EditUser user={this.state.user} onChange={this.setUser} />
         );
     }
 })
