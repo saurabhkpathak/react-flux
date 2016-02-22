0111a2a2b2882dbc95be49e04ffa7048806b6368
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-22 15:16:26 +0530

    creating reusable input box

---
+++ src/components/common/textInput.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var React = require('react');
+
+var TextInput = React.createClass({
+    propTypes: {
+        name: React.PropTypes.string.isRequired,
+        type: React.PropTypes.string.isRequired,
+        placeholder: React.PropTypes.string,
+        onChange: React.PropTypes.func.isRequired,
+        value: React.PropTypes.string
+    },
+    render: function() {
+        return (
+            <div className="col-md-12">
+                <label>{this.props.name}</label>
+                <input type={this.props.type} placeholder={this.props.placeholder} className="form-control" name={this.props.name} value={this.props.value} onChange={this.props.onChange} />
+            </div>
+        );
+    }
+});
+
+module.exports = TextInput;
