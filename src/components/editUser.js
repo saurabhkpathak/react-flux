1ea930704fcd5877bec0e14f2252294c632e65ac
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-22 13:58:26 +0530

    adding default values and onChange function

--- src/components/editUser.js
+++ src/components/editUser.js
@@ -7,9 +7,9 @@ var EditUser = React.createClass({
         return (
             <form>
                 <label>Name</label>
-                <input type="text" placeholder="Name" className="form-control" />
+                <input type="text" placeholder="Name" className="form-control" name="name" value={this.props.user.name} onChange={this.props.onChange} />
                 <label>Phone</label>
-                <input type="number" placeholder="Phone" className="form-control" />
+                <input type="text" placeholder="Phone" className="form-control" name="phone" value={this.props.user.phone} onChange={this.props.onChange} />
                 <button className="btn btn-primary">Submit</button>
             </form>
         );
