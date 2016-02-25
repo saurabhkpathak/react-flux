ea39e967cbddf6899da34bf2d67ee01b8eac1c0f
saurabhkpathak <saurabhkpathak@yahoo.com>
2016-02-25 12:55:12 +0530

    initializing error state and adding form validation function

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -23,7 +23,8 @@ var ManageUser = React.createClass({
     getInitialState: function() {
         this.getUserDetails();
         return {
-            user: {}
+            user: {},
+            error: {}
         }
     },
     setUser: function(event) {
@@ -34,15 +35,34 @@ var ManageUser = React.createClass({
     },
     saveUser: function(event) {
         event.preventDefault();
+        if (!this.isFormValid()) {
+            return;
+        }
         ApiWrapper.saveUser(this.state.user).done(function() {
             toastr.success('user saved');
         }).fail(function() {
             toastr.error('user save failed');
         });
     },
+    isFormValid: function() {
+        var valid = true;
+        this.state.error = {};
+        if (this.state.user.name.length < 3) {
+            this.state.error.name = 'Name cannot be shorted than 3 characters.';
+            valid = false;
+        }
+        if (this.state.user.phone.length !== 10) {
+            this.state.error.phone = 'Phone number cannot be shorted than 10 characters.';
+            valid = false;
+        }
+        this.setState({
+            error: this.state.error
+        });
+        return valid;
+    },
     render: function() {
         return (
-            <EditUser user={this.state.user} onChange={this.setUser} onSave={this.saveUser}/>
+            <EditUser user={this.state.user} onChange={this.setUser} onSave={this.saveUser} error={this.state.error}/>
         );
     }
 })
